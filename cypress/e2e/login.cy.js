345b9e6b0cc5887adc2a5f70266e6414d72c82f3
Guileomi <guih.sousa123@gmail.com>
2026-07-11 20:08:06 -0300

    implementa testes de locin com fixtures e variáveis de ambiente

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,22 +1,28 @@
 describe('Login', () => {
-  it('Login com dados válidos deve ser realizado com sucesso', () => {
+  beforeEach(() => {
     //Arrange
-    cy.visit('http://localhost:4000')
+    cy.visit(Cypress.env('URL'))
+  })
 
+  it('Login com dados válidos deve ser realizado com sucesso', () => {
+    
+    cy.fixture('credenciais').then((credenciais) => {
     //Act
-    cy.get('#username').click().type('admin')
-    cy.get('#senha').click().type('senha123')
+    cy.get('#username').click().type(credenciais.validos.usuario)
+    cy.get('#senha').click().type(credenciais.validos.senha)
+    })
     cy.contains('button', 'Entrar').click()
 
     //Assert
-    cy.contains('h4', 'Realizar transf').should('be.visible')
+    cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados inválidos deve exibir mensagem de erro', () => {
-    cy.visit('http://localhost:4000')
-
-    cy.get('#username').click().type('admin')
-    cy.get('#senha').click().type('111111')
+    cy.fixture('credenciais').then((credenciais) => {
+    //Act
+    cy.get('#username').click().type(credenciais.invalidos.usuario)
+    cy.get('#senha').click().type(credenciais.invalidos.senha)
+    })
     cy.contains('button', 'Entrar').click()
   
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
